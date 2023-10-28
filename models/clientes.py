daf731d1ee4e79df884cdae77cd3bea234117685
Carlos Marchante Caballero <127337920+marchanteHacker@users.noreply.github.com>
2023-10-28 10:37:27 +0200

    Add files via upload

--- models/clientes.py
+++ models/clientes.py
@@ -1,1 +1,17 @@
+from odoo import models, fields, api
 
+
+class Clientes(models.Model):
+     _name = 'peluqueria.clientes'
+     _description = 'Clientes de la peluquería.'
+
+     name = fields.Char(string="Title", required=True, help="Nombre del cliente")
+     apellidos = fields.Char(string="Title", required=True, help="Apellidos del cliente")
+     telefono  = fields.Integer()
+     dni = fields.Char()
+     correoElectronico = fields.Char(string="Title", required=True, help="Correo Electronico del cliente")
+     
+     
+     citas_ids = fields.Many2one("peluqueria.citas",string="Confirmar cita")
+     
+     
