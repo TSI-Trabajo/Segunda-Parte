2380eda64c0c20435c99e22097f6b849930387ab
joseantonio_or <48123924+orozcorodrijoseantonio@users.noreply.github.com>
2023-10-30 13:19:44 +0100

    Add files via upload

---
+++ upobarber/models/MetodoPago.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class MetodoPago(models.Model):
+    _name = 'upobarber.metodopago'
+    _description = 'upobarber MetodoPago'
+
+    metodo = fields.boolean(string="Metodo Pago", required=True, help="Metodo del Pago")
