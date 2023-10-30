40123e15f8a5b678226ffb6ad9a7b12adcf8abd9
joseantonio_or <48123924+orozcorodrijoseantonio@users.noreply.github.com>
2023-10-30 20:09:30 +0100

    Delete upobarber directory

--- upobarber/models/MetodoPago.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class MetodoPago(models.Model):
-    _name = 'upobarber.metodopago'
-    _description = 'upobarber MetodoPago'
-
-    metodo = fields.boolean(string="Metodo Pago", required=True, help="Metodo del Pago")
